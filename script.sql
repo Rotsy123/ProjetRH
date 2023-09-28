18d73fb43cf69f7876d4c9b1dc5cc1f4ead1ecd1
Kanto-Andrianotahina <andrianotahinak@gmail.com>
2023-09-28 15:57:44 +0300

    Script

---
+++ script.sql
@@ -0,0 +1,75 @@
+create database rh
+create table departement(
+    idDepartement int primary key,
+    nomDepartement varchar(30)
+);
+
+create table demande(
+    idDemande int primary key,
+    dateDemande date,
+    heureTravaux datetime,
+    hommeJour double precision,
+    idDepartement int,
+    foreign key (idDepartement) references departement(idDepartement)
+);
+
+create table poste(
+    idPoste int primary key,
+    nomPoste varchar(30),
+    effectif int,
+    idDemande int,
+    dateFinPostule datetime,
+    foreign key (idDemande) references demande(idDemande)
+);
+create table employes(
+    idEmp int primary key,
+    idDept int,
+    idPoste int,
+    salaire double precision,
+    foreign key (idDepartement) references departement(idDepartement),
+    foreign key (idPPoste) references poste(idPoste),
+);
+
+
+-- create table critere(
+--     idCritere int primary key,
+--     nomCritere varchar(30)
+-- );
+
+-- create table annonce(
+--     idAnnonce int,
+--     idCritere int,
+--     coefficient int,
+--     idPoste int,
+--     foreign key (idCritere) references critere(idCritere),
+--     foreign key (idPoste) references poste(idPoste)
+-- ); 
+
+-- create table candidat(
+--     idCandidat int primary key,
+--     nomCandidat varchar(30), 
+--     retenu int,
+--     datePostule datetime
+-- );
+
+-- create table notecritere(
+--     idCandidat int, 
+--     idCritere int,
+--     note double precision,
+--     foreign key (idCandidat) references candidat(idCandidat)
+--     foreign key (idCritere) references critere(idCritere)
+-- );
+
+-- create table teste(
+--     idTeste int,
+--     idPoste int,
+--     questions
+
+-- );
+
+-- create table notetest(
+--     idCandidat int, 
+--     idTeste int,
+--     note double precision,
+--     foreign key (idCandidat) references candidat(idCandidat)
+-- );
